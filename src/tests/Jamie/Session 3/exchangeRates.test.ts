89e162834f742041ef597a781da9304bc793d586
jamie.julius <jamie.julius@sundaysky.com>
2020-11-11 18:16:56 +0200

    Homework for session 3

--- src/tests/Jamie/Session 3/exchangeRates.test.ts
+++ src/tests/Jamie/Session 3/exchangeRates.test.ts
@@ -1,18 +1,55 @@
 import { getExchangeRates } from "../../../app/Services/exchangeRates";
 
 describe("Exchange Rates Service", () => {
+    const originalFetch = global.fetch;
+    const mockedFetch = jest.fn();
+
+    beforeAll(() => {
+        global.fetch = mockedFetch
+    });
+
+    afterAll(() => {
+        global.fetch = originalFetch;
+    });
+
+    beforeEach(() => {
+        mockedFetch.mockClear();
+    });
+
     it("should return rates if all okay", async () => {
+        mockedFetch.mockImplementationOnce(() => Promise.resolve({
+            json: () => Promise.resolve({rates: { USD: 3.42 }})
+        }));
+
         const rates = await getExchangeRates();
-        expect(rates.USD).toBeDefined();
+        expect(rates.USD).toBe(3.42);
     });
 
     it("should throw an error on connection issues", async () => {
-        const rates = await getExchangeRates();
-        expect(rates.USD).toBeDefined();
+        mockedFetch.mockImplementationOnce(() => Promise.reject("DNS Lookup Failed"));
+
+        expect.assertions(1);
+
+        try {
+            await getExchangeRates();
+        }
+        catch (error) {
+            expect(error).toBeDefined();
+        }
     });
 
     it("should throw an error on unexpected response", async () => {
-        const rates = await getExchangeRates();
-        expect(rates.USD).toBeDefined();
+        mockedFetch.mockImplementationOnce(() => Promise.resolve({
+            json: () => Promise.resolve({colors: { primary: "red" }})
+        }));
+
+        expect.assertions(1);
+
+        try {
+            await getExchangeRates();
+        }
+        catch (error) {
+            expect(error).toBeDefined();
+        }
     });
 });
